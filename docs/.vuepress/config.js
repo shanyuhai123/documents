10b0952e90c13b95eaf93b69f901a9edc80dd847
shanyuhai123 <864299347@qq.com>
2019-02-16 23:28:22 +0800

    feat: add different formats of Icons(manifest, favicons)

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -11,8 +11,10 @@ module.exports = {
     }
   },
   head: [
-    ['link', { rel: 'icon', href: '/favicon.png' }],
-    ['link', { rel: 'manifest', href: '/manifest.json' }]
+    ['link', { rel: 'apple-touch-icon', href: '/apple-touch-icon.png' }],
+    ['link', { rel: 'icon', href: '/favicon-32x32.png' }],
+    ['link', { rel: 'manifest', href: '/manifest.json' }],
+    ['meta', { name: 'theme-color', content: '#ffffff' }],
   ],
   plugins: pluginConf,
   themeConfig: {
