78ddc185c3fbe5d973f7e715d573f4a5c9de1a45
shanyuhai <864299347@qq.com>
2019-04-23 09:02:58 +0800

    docs(nginx)：Ubuntu 下安装最新版本 Nginx

--- config/sidebarConf/BackEnd/nginx/index.js
+++ config/sidebarConf/BackEnd/nginx/index.js
@@ -1,5 +1,5 @@
 const utils = require('../../../../utils/index.js');
-const children = ['','01-install-nginx','02-explain-the-main-configuration-file-of-nginx','03-nginx-basic-operation'];
+const children = ['','01-install-nginx','02-explain-the-main-configuration-file-of-nginx','03-nginx-basic-operation','04-ubuntu-installs-the-latest-version-of-nginx'];
 
 module.exports = [
   utils.genSidebar('Nginx', children, false),
