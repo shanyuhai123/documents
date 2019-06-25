c031bd979d3a7980ce853d6a8538c4aecb1bccc1
shanyuhai123 <864299347@qq.com>
2019-06-25 18:42:06 +0800

    fix: 将秘钥移出

---
+++ config/secretKeyConf.js
@@ -0,0 +1,6 @@
+module.exports = secretKeyConf = {
+  appId: 's66rnnGxseSiRy74nH6zckU6-gzGzoHsz',
+  appKey: 'DqBkfix1mnxRNAxCDgQgnxbu',
+  ga: 'UA-134613928-1',
+  googleSearchConsole: 'Ld_AEWr30siza2LmmZ12csitRGsSj9gqqEAch0UXkTc',
+}
