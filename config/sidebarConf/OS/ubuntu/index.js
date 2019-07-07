f58afa18e584de6d471c727666ee681775cd5845
shanyuhai123 <864299347@qq.com>
2019-07-08 01:16:09 +0800

    docs(ubuntu): 新增 Ubuntu Server 修复时间

--- config/sidebarConf/OS/ubuntu/index.js
+++ config/sidebarConf/OS/ubuntu/index.js
@@ -1,5 +1,5 @@
 const utils = require('../../../../utils/index.js');
-const children = ['','01-add-user','02-login-with-rsa-key','03-upload-file-to-server','04-clone-project-to-server'];
+const children = ['','01-add-user','02-login-with-rsa-key','03-upload-file-to-server','04-clone-project-to-server','05-fix-date'];
 
 module.exports = [
   utils.genSidebar('Ubuntu', children, false),
---
+++ docs/OS/ubuntu/05-fix-date.md
@@ -0,0 +1,23 @@
+---
+title: 修复时间显示
+---
+
+## 介绍
+
+初步安装系统后日期时间显示的并非当前时区。
+
+## 解决方案
+
+```sh
+# 1. 显示时间
+date -R
+# Sun, 07 Jul 2019 xx:xx:xx +0000
+
+# 2. 修改时区
+sudo tzselect
+# 按照提示修改即可
+
+# 3. 拷贝文件到本地时间
+sudo cp /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+# 再次执行 date -R 即可看到正确时间
+```
