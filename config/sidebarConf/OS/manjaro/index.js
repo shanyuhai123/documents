ad21266867ad3b811f7b93b03af08bed0977a58e
shanyuhai123 <864299347@qq.com>
2019-07-10 23:14:39 +0800

    docs(manjaro): 新增 虚拟机安装 Manjaro 说明

--- config/sidebarConf/OS/manjaro/index.js
+++ config/sidebarConf/OS/manjaro/index.js
@@ -1,5 +1,5 @@
 const utils = require('../../../../utils/index.js');
-const children = ['','01-manjaro-configuration','02-solve-problems-with-manjaro','03-most-used-softwares'];
+const children = ['','01-how-to-install-manjaro-on-virtualbox','02-manjaro-configuration','03-solve-problems-with-manjaro','04-most-used-softwares'];
 
 module.exports = [
   utils.genSidebar('Manjaro', children, false),
