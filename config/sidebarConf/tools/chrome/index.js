f3dd83cf03b227528857670e1ce9687bf7638a9c
shanyuhai123 <864299347@qq.com>
2019-09-24 23:32:29 +0800

    docs(chrome): 新增 实时保存文件

--- config/sidebarConf/tools/chrome/index.js
+++ config/sidebarConf/tools/chrome/index.js
@@ -1,6 +1,6 @@
 const genSidebar = require('../../../../utils/genSidebar.js');
 const filenames = require('../../../filenames.json');
-const children = filenames.tools.vscode;
+const children = filenames.tools.chrome;
 
 module.exports = [
   genSidebar('Chrome Developer Tools', children, false),
