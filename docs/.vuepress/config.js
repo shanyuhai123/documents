5026713d6c7d5fa1f5bee9c5bde1084189fc6613
shanyuhai123 <864299347@qq.com>
2020-12-11 22:17:07 +0800

    chore: 移除 algolia

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -13,15 +13,6 @@ module.exports = {
     editLinks: true,
     editLinkText: '编辑文档！',
     docsDir: 'docs',
-    nav: navConf,
-    algolia: {
-      appId: 'M698VCXCJN',
-      apiKey: '8b552055fb68ffc808bfbd3f98a1dbe2',
-      indexName: 'shanyuhai_documents',
-      algoliaOptions: {
-        hitsPerPage: 10,
-        facetFilters: ""
-      }
-    }
+    nav: navConf
   },
 }
