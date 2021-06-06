e8a952c795801fda1e06add51145f7d44184c73a
shanyuhai123 <864299347@qq.com>
2021-06-06 11:49:20 +0800

    chore: 更新 nav

--- docs/.vuepress/config/navConf.js
+++ docs/.vuepress/config/navConf.js
@@ -52,6 +52,7 @@ module.exports = [
   ]},
   { text: '更多', items: [
     { text: '导航 🎉', link: '/更多🎉/navigation' },
+    { text: 'VuePress 侧边栏插件', link: 'https://shanyuhai123.github.io/vuepress-plugin-auto-sidebar/zh/' },
     { text: 'VuePress 官网', link: 'https://vuepress.vuejs.org/zh/' },
   ]}
 ];
