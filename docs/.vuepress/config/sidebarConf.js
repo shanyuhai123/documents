ad14facbaeb8a93c42812519543a1e2bf5f7a010
shanyuhai123 <864299347@qq.com>
2021-09-11 23:36:56 +0800

    chore: update sidebar

--- docs/.vuepress/config/sidebarConf.js
+++ docs/.vuepress/config/sidebarConf.js
@@ -1,15 +1,4 @@
 module.exports = {
-  "/algorithms/dynamic-programming/": [
-    {
-      "text": "DYNAMIC-PROGRAMMING",
-      "title": "DYNAMIC-PROGRAMMING",
-      "collapsable": false,
-      "sidebarDepth": 1,
-      "children": [
-        "README.md"
-      ]
-    }
-  ],
   "/algorithms/divide-and-conquer/": [
     {
       "text": "DIVIDE-AND-CONQUER",
@@ -21,10 +10,249 @@ module.exports = {
       ]
     }
   ],
-  "/tools/bookmark-scripts/": [
+  "/algorithms/dynamic-programming/": [
     {
-      "text": "BOOKMARK-SCRIPTS",
-      "title": "BOOKMARK-SCRIPTS",
+      "text": "DYNAMIC-PROGRAMMING",
+      "title": "DYNAMIC-PROGRAMMING",
+      "collapsable": false,
+      "sidebarDepth": 1,
+      "children": [
+        "README.md"
+      ]
+    }
+  ],
+  "/algorithms/leetcode/": [
+    {
+      "text": "LEETCODE",
+      "title": "LEETCODE",
+      "collapsable": false,
+      "sidebarDepth": 1,
+      "children": [
+        "README.md",
+        "1-two-sum.md"
+      ]
+    }
+  ],
+  "/algorithms/sort/": [
+    {
+      "text": "SORT",
+      "title": "SORT",
+      "collapsable": false,
+      "sidebarDepth": 1,
+      "children": [
+        "README.md"
+      ]
+    }
+  ],
+  "/backend/docker/": [
+    {
+      "text": "DOCKER",
+      "title": "DOCKER",
+      "collapsable": false,
+      "sidebarDepth": 1,
+      "children": [
+        "README.md",
+        "dockerfile.md",
+        "image.md",
+        "container.md",
+        "volumes.md",
+        "docker-compose.md",
+        "etcd.md",
+        "private-registry.md"
+      ]
+    }
+  ],
+  "/backend/golang/": [
+    {
+      "text": "GOLANG",
+      "title": "GOLANG",
+      "collapsable": false,
+      "sidebarDepth": 1,
+      "children": [
+        "README.md"
+      ]
+    }
+  ],
+  "/backend/http/": [
+    {
+      "text": "HTTP",
+      "title": "HTTP",
+      "collapsable": false,
+      "sidebarDepth": 1,
+      "children": [
+        "README.md"
+      ]
+    }
+  ],
+  "/backend/mongodb/": [
+    {
+      "text": "MONGODB",
+      "title": "MONGODB",
+      "collapsable": false,
+      "sidebarDepth": 1,
+      "children": [
+        "README.md"
+      ]
+    }
+  ],
+  "/backend/mysql/": [
+    {
+      "text": "MYSQL",
+      "title": "MYSQL",
+      "collapsable": false,
+      "sidebarDepth": 1,
+      "children": [
+        "README.md",
+        "insert-update-and-delete.md",
+        "join-and-union.md",
+        "select.md",
+        "stored-procedure.md",
+        "transactions.md",
+        "trigger.md",
+        "views.md"
+      ]
+    }
+  ],
+  "/backend/nestjs/": [
+    {
+      "text": "NESTJS",
+      "title": "NESTJS",
+      "collapsable": false,
+      "sidebarDepth": 1,
+      "children": [
+        "README.md",
+        "architecture.md",
+        "typeorm.md"
+      ]
+    }
+  ],
+  "/backend/nginx/": [
+    {
+      "text": "NGINX",
+      "title": "NGINX",
+      "collapsable": false,
+      "sidebarDepth": 1,
+      "children": [
+        "README.md",
+        "configurin-systemd-service-for-nginx.md",
+        "handle-error.md",
+        "install-goaccess.md",
+        "limit-module.md",
+        "location-rules.md",
+        "master-slave-nginx.md",
+        "nginx-upstream.md",
+        "proxy-pass-rules.md",
+        "referer-module.md",
+        "rewrite-rules.md",
+        "the-difference-between-root-and-alias.md",
+        "upgrade-http-to-https.md",
+        "websocket-proxy.md"
+      ]
+    }
+  ],
+  "/backend/nodejs/": [
+    {
+      "text": "NODEJS",
+      "title": "NODEJS",
+      "collapsable": false,
+      "sidebarDepth": 1,
+      "children": [
+        "README.md",
+        "lerna.md",
+        "npm-and-yarn-commands.md",
+        "use-verdaccio-to-deploy-private-npm.md"
+      ]
+    }
+  ],
+  "/backend/redis/": [
+    {
+      "text": "REDIS",
+      "title": "REDIS",
+      "collapsable": false,
+      "sidebarDepth": 1,
+      "children": [
+        "README.md"
+      ]
+    }
+  ],
+  "/backend/traefik/": [
+    {
+      "text": "TRAEFIK",
+      "title": "TRAEFIK",
+      "collapsable": false,
+      "sidebarDepth": 1,
+      "children": [
+        "README.md",
+        "traefik-with-frp.md"
+      ]
+    }
+  ],
+  "/cornerstone/math/": [
+    {
+      "text": "MATH",
+      "title": "MATH",
+      "collapsable": false,
+      "sidebarDepth": 1,
+      "children": [
+        "README.md"
+      ]
+    }
+  ],
+  "/design/pixel/": [
+    {
+      "text": "PIXEL",
+      "title": "PIXEL",
+      "collapsable": false,
+      "sidebarDepth": 1,
+      "children": [
+        "README.md",
+        "install-aseprite-on-linux.md"
+      ]
+    }
+  ],
+  "/frontend/canvas/": [
+    {
+      "text": "CANVAS",
+      "title": "CANVAS",
+      "collapsable": false,
+      "sidebarDepth": 1,
+      "children": [
+        "README.md",
+        "graphics.md"
+      ]
+    }
+  ],
+  "/frontend/css/": [
+    {
+      "text": "CSS",
+      "title": "CSS",
+      "collapsable": false,
+      "sidebarDepth": 1,
+      "children": [
+        "README.md",
+        "@font-face.md",
+        "anchor.md",
+        "bfc.md",
+        "margin.md"
+      ]
+    }
+  ],
+  "/frontend/d3js/": [
+    {
+      "text": "D3JS",
+      "title": "D3JS",
+      "collapsable": false,
+      "sidebarDepth": 1,
+      "children": [
+        "README.md",
+        "hello-d3.md"
+      ]
+    }
+  ],
+  "/frontend/flutter/": [
+    {
+      "text": "FLUTTER",
+      "title": "FLUTTER",
       "collapsable": false,
       "sidebarDepth": 1,
       "children": [
@@ -63,100 +291,14 @@ module.exports = {
       ]
     }
   ],
-  "/frontend/typescript/": [
+  "/frontend/jest/": [
     {
-      "text": "TYPESCRIPT",
-      "title": "TYPESCRIPT",
-      "collapsable": false,
-      "sidebarDepth": 1,
-      "children": [
-        "README.md",
-        "change-axios-response-type.md",
-        "eslint-do-not-work-correctly-with-ts.md",
-        "extend-vue-router-meta-interface.md",
-        "type-challenges.md",
-        "typescript-normal-problems.md"
-      ]
-    }
-  ],
-  "/cornerstone/math/": [
-    {
-      "text": "MATH",
-      "title": "MATH",
+      "text": "JEST",
+      "title": "JEST",
       "collapsable": false,
       "sidebarDepth": 1,
       "children": [
         "README.md"
-      ]
-    }
-  ],
-  "/frontend/flutter/": [
-    {
-      "text": "FLUTTER",
-      "title": "FLUTTER",
-      "collapsable": false,
-      "sidebarDepth": 1,
-      "children": [
-        "README.md"
-      ]
-    }
-  ],
-  "/frontend/webpack/": [
-    {
-      "text": "WEBPACK",
-      "title": "WEBPACK",
-      "collapsable": false,
-      "sidebarDepth": 1,
-      "children": [
-        "README.md",
-        "loader.md",
-        "simple-bundle-analysis.md"
-      ]
-    }
-  ],
-  "/algorithms/sort/": [
-    {
-      "text": "SORT",
-      "title": "SORT",
-      "collapsable": false,
-      "sidebarDepth": 1,
-      "children": [
-        "README.md"
-      ]
-    }
-  ],
-  "/backend/mongodb/": [
-    {
-      "text": "MONGODB",
-      "title": "MONGODB",
-      "collapsable": false,
-      "sidebarDepth": 1,
-      "children": [
-        "README.md"
-      ]
-    }
-  ],
-  "/backend/traefik/": [
-    {
-      "text": "TRAEFIK",
-      "title": "TRAEFIK",
-      "collapsable": false,
-      "sidebarDepth": 1,
-      "children": [
-        "README.md",
-        "traefik-with-frp.md"
-      ]
-    }
-  ],
-  "/frontend/canvas/": [
-    {
-      "text": "CANVAS",
-      "title": "CANVAS",
-      "collapsable": false,
-      "sidebarDepth": 1,
-      "children": [
-        "README.md",
-        "graphics.md"
       ]
     }
   ],
@@ -171,59 +313,19 @@ module.exports = {
       ]
     }
   ],
-  "/backend/docker/": [
+  "/frontend/typescript/": [
     {
-      "text": "DOCKER",
-      "title": "DOCKER",
+      "text": "TYPESCRIPT",
+      "title": "TYPESCRIPT",
       "collapsable": false,
       "sidebarDepth": 1,
       "children": [
         "README.md",
-        "dockerfile.md",
-        "image.md",
-        "container.md",
-        "volumes.md",
-        "docker-compose.md",
-        "etcd.md",
-        "private-registry.md"
-      ]
-    }
-  ],
-  "/backend/golang/": [
-    {
-      "text": "GOLANG",
-      "title": "GOLANG",
-      "collapsable": false,
-      "sidebarDepth": 1,
-      "children": [
-        "README.md"
-      ]
-    }
-  ],
-  "/backend/nestjs/": [
-    {
-      "text": "NESTJS",
-      "title": "NESTJS",
-      "collapsable": false,
-      "sidebarDepth": 1,
-      "children": [
-        "README.md",
-        "architecture.md",
-        "typeorm.md"
-      ]
-    }
-  ],
-  "/backend/nodejs/": [
-    {
-      "text": "NODEJS",
-      "title": "NODEJS",
-      "collapsable": false,
-      "sidebarDepth": 1,
-      "children": [
-        "README.md",
-        "lerna.md",
-        "npm-and-yarn-commands.md",
-        "use-verdaccio-to-deploy-private-npm.md"
+        "change-axios-response-type.md",
+        "eslint-do-not-work-correctly-with-ts.md",
+        "extend-vue-router-meta-interface.md",
+        "type-challenges.md",
+        "typescript-normal-problems.md"
       ]
     }
   ],
@@ -247,6 +349,29 @@ module.exports = {
       ]
     }
   ],
+  "/frontend/vite/": [
+    {
+      "text": "VITE",
+      "title": "VITE",
+      "collapsable": false,
+      "sidebarDepth": 1,
+      "children": [
+        "README.md"
+      ]
+    }
+  ],
+  "/frontend/vue/": [
+    {
+      "text": "VUE",
+      "title": "VUE",
+      "collapsable": false,
+      "sidebarDepth": 1,
+      "children": [
+        "README.md",
+        "ref-type.md"
+      ]
+    }
+  ],
   "/frontend/weapp/": [
     {
       "text": "WEAPP",
@@ -258,90 +383,30 @@ module.exports = {
       ]
     }
   ],
-  "/backend/mysql/": [
+  "/frontend/webpack/": [
     {
-      "text": "MYSQL",
-      "title": "MYSQL",
+      "text": "WEBPACK",
+      "title": "WEBPACK",
       "collapsable": false,
       "sidebarDepth": 1,
       "children": [
         "README.md",
-        "insert-update-and-delete.md",
-        "join-and-union.md",
-        "select.md",
-        "stored-procedure.md",
-        "transactions.md",
-        "trigger.md",
-        "views.md"
+        "loader.md",
+        "simple-bundle-analysis.md"
       ]
     }
   ],
-  "/backend/nginx/": [
+  "/os/centos/": [
     {
-      "text": "NGINX",
-      "title": "NGINX",
+      "text": "CENTOS",
+      "title": "CENTOS",
       "collapsable": false,
       "sidebarDepth": 1,
       "children": [
         "README.md",
-        "configurin-systemd-service-for-nginx.md",
-        "handle-error.md",
-        "install-goaccess.md",
-        "limit-module.md",
-        "location-rules.md",
-        "master-slave-nginx.md",
-        "nginx-upstream.md",
-        "proxy-pass-rules.md",
-        "referer-module.md",
-        "rewrite-rules.md",
-        "the-difference-between-root-and-alias.md",
-        "upgrade-http-to-https.md",
-        "websocket-proxy.md"
-      ]
-    }
-  ],
-  "/backend/redis/": [
-    {
-      "text": "REDIS",
-      "title": "REDIS",
-      "collapsable": false,
-      "sidebarDepth": 1,
-      "children": [
-        "README.md"
-      ]
-    }
-  ],
-  "/frontend/d3js/": [
-    {
-      "text": "D3JS",
-      "title": "D3JS",
-      "collapsable": false,
-      "sidebarDepth": 1,
-      "children": [
-        "README.md",
-        "hello-d3.md"
-      ]
-    }
-  ],
-  "/frontend/jest/": [
-    {
-      "text": "JEST",
-      "title": "JEST",
-      "collapsable": false,
-      "sidebarDepth": 1,
-      "children": [
-        "README.md"
-      ]
-    }
-  ],
-  "/frontend/vite/": [
-    {
-      "text": "VITE",
-      "title": "VITE",
-      "collapsable": false,
-      "sidebarDepth": 1,
-      "children": [
-        "README.md"
+        "you-need-to-know-yum.md",
+        "install-the-latest-version-of-nginx.md",
+        "modify-the-network-named-eth.md"
       ]
     }
   ],
@@ -360,53 +425,94 @@ module.exports = {
       ]
     }
   ],
-  "/backend/http/": [
+  "/os/linux/": [
     {
-      "text": "HTTP",
-      "title": "HTTP",
+      "text": "LINUX",
+      "title": "LINUX",
+      "collapsable": false,
+      "sidebarDepth": 1,
+      "children": [
+        "README.md",
+        "everything-is-a-file.md",
+        "alias.md",
+        "ansible.md",
+        "archiving-and-compression.md",
+        "command-options.md",
+        "command-shortcut.md",
+        "common-commands.md",
+        "crontab.md",
+        "detect-your-server.md",
+        "directory-structure.md",
+        "dnsmasq.md",
+        "fail2ban.md",
+        "firewalld.md",
+        "gaudy-and-pragmatical-command.md",
+        "grep-sed-awk.md",
+        "how-to-enable-the-hard-disk.md",
+        "io-redirection.md",
+        "job-control.md",
+        "prometheus.md",
+        "raid.md",
+        "rsync-and-scp.md",
+        "ssh-makes-your-server-more-secure.md",
+        "systemd.md",
+        "user.md",
+        "view-text-context.md",
+        "xargs.md"
+      ]
+    }
+  ],
+  "/os/manjaro/": [
+    {
+      "text": "MANJARO",
+      "title": "MANJARO",
+      "collapsable": false,
+      "sidebarDepth": 1,
+      "children": [
+        "README.md",
+        "archlinux-aur.md",
+        "enhance-virtualbox-tools-on-manjaro.md",
+        "fix-idea.md",
+        "install-deb-program-on-manjaro.md",
+        "konsole-profile-command.md",
+        "lock-screen-shortcuts.md",
+        "most-used-softwares.md",
+        "mpv.md",
+        "proxy-tools.md",
+        "readonly-after-mount-the-disk.md",
+        "solve-problems-with-manjaro.md",
+        "terminal-agent.md",
+        "you-get.md",
+        "zsh-can-not-find-npm.md"
+      ]
+    }
+  ],
+  "/os/ubuntu/": [
+    {
+      "text": "UBUNTU",
+      "title": "UBUNTU",
+      "collapsable": false,
+      "sidebarDepth": 1,
+      "children": [
+        "README.md",
+        "change-swap-size.md",
+        "config-netplan.md",
+        "fix-date.md",
+        "frp.md",
+        "frp-and-nginx-share-80-port.md",
+        "install-the-latest-version-of-nginx.md",
+        "ubuntu-computer-config.md"
+      ]
+    }
+  ],
+  "/tools/bookmark-scripts/": [
+    {
+      "text": "BOOKMARK-SCRIPTS",
+      "title": "BOOKMARK-SCRIPTS",
       "collapsable": false,
       "sidebarDepth": 1,
       "children": [
         "README.md"
-      ]
-    }
-  ],
-  "/design/pixel/": [
-    {
-      "text": "PIXEL",
-      "title": "PIXEL",
-      "collapsable": false,
-      "sidebarDepth": 1,
-      "children": [
-        "README.md",
-        "install-aseprite-on-linux.md"
-      ]
-    }
-  ],
-  "/frontend/css/": [
-    {
-      "text": "CSS",
-      "title": "CSS",
-      "collapsable": false,
-      "sidebarDepth": 1,
-      "children": [
-        "README.md",
-        "@font-face.md",
-        "anchor.md",
-        "bfc.md",
-        "margin.md"
-      ]
-    }
-  ],
-  "/frontend/vue/": [
-    {
-      "text": "VUE",
-      "title": "VUE",
-      "collapsable": false,
-      "sidebarDepth": 1,
-      "children": [
-        "README.md",
-        "ref-type.md"
       ]
     }
   ],
@@ -420,6 +526,20 @@ module.exports = {
         "README.md",
         "edit-file-with-workspaces.md",
         "user-friendly-settings.md"
+      ]
+    }
+  ],
+  "/tools/git/": [
+    {
+      "text": "GIT",
+      "title": "GIT",
+      "collapsable": false,
+      "sidebarDepth": 1,
+      "children": [
+        "README.md",
+        "commit-message.md",
+        "gitignore.md",
+        "submodule.md"
       ]
     }
   ],
@@ -471,77 +591,6 @@ module.exports = {
       ]
     }
   ],
-  "/os/manjaro/": [
-    {
-      "text": "MANJARO",
-      "title": "MANJARO",
-      "collapsable": false,
-      "sidebarDepth": 1,
-      "children": [
-        "README.md",
-        "archlinux-aur.md",
-        "enhance-virtualbox-tools-on-manjaro.md",
-        "fix-idea.md",
-        "install-deb-program-on-manjaro.md",
-        "konsole-profile-command.md",
-        "lock-screen-shortcuts.md",
-        "most-used-softwares.md",
-        "mpv.md",
-        "proxy-tools.md",
-        "readonly-after-mount-the-disk.md",
-        "solve-problems-with-manjaro.md",
-        "terminal-agent.md",
-        "you-get.md",
-        "zsh-can-not-find-npm.md"
-      ]
-    }
-  ],
-  "/os/centos/": [
-    {
-      "text": "CENTOS",
-      "title": "CENTOS",
-      "collapsable": false,
-      "sidebarDepth": 1,
-      "children": [
-        "README.md",
-        "you-need-to-know-yum.md",
-        "install-the-latest-version-of-nginx.md",
-        "modify-the-network-named-eth.md"
-      ]
-    }
-  ],
-  "/os/ubuntu/": [
-    {
-      "text": "UBUNTU",
-      "title": "UBUNTU",
-      "collapsable": false,
-      "sidebarDepth": 1,
-      "children": [
-        "README.md",
-        "change-swap-size.md",
-        "config-netplan.md",
-        "fix-date.md",
-        "frp.md",
-        "frp-and-nginx-share-80-port.md",
-        "install-the-latest-version-of-nginx.md",
-        "ubuntu-computer-config.md"
-      ]
-    }
-  ],
-  "/tools/git/": [
-    {
-      "text": "GIT",
-      "title": "GIT",
-      "collapsable": false,
-      "sidebarDepth": 1,
-      "children": [
-        "README.md",
-        "commit-message.md",
-        "gitignore.md",
-        "submodule.md"
-      ]
-    }
-  ],
   "/computer/": [
     {
       "text": "COMPUTER",
@@ -556,43 +605,6 @@ module.exports = {
         "nat.md",
         "network-architecture.md",
         "router.md"
-      ]
-    }
-  ],
-  "/os/linux/": [
-    {
-      "text": "LINUX",
-      "title": "LINUX",
-      "collapsable": false,
-      "sidebarDepth": 1,
-      "children": [
-        "README.md",
-        "everything-is-a-file.md",
-        "alias.md",
-        "ansible.md",
-        "archiving-and-compression.md",
-        "command-options.md",
-        "command-shortcut.md",
-        "common-commands.md",
-        "crontab.md",
-        "detect-your-server.md",
-        "directory-structure.md",
-        "dnsmasq.md",
-        "fail2ban.md",
-        "firewalld.md",
-        "gaudy-and-pragmatical-command.md",
-        "grep-sed-awk.md",
-        "how-to-enable-the-hard-disk.md",
-        "io-redirection.md",
-        "job-control.md",
-        "prometheus.md",
-        "raid.md",
-        "rsync-and-scp.md",
-        "ssh-makes-your-server-more-secure.md",
-        "systemd.md",
-        "user.md",
-        "view-text-context.md",
-        "xargs.md"
       ]
     }
   ],
