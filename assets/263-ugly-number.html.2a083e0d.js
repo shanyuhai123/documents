7e6a79b4174f0a76d3f13566d3886aa1afe5bc0a
shanyuhai123 <shanyuhai123@users.noreply.github.com>
2021-11-17 14:30:17 +0000

    docs(linux): init shell 35a1c5adb9532fa851b57d1197e9f573df317d9c

---
+++ assets/263-ugly-number.html.2a083e0d.js
@@ -0,0 +1,14 @@
+import{e as n}from"./app.cc89d5d0.js";import{_ as s}from"./plugin-vue_export-helper.5a098b48.js";const a={},p=n(`<h1 id="\u4E11\u6570" tabindex="-1"><a class="header-anchor" href="#\u4E11\u6570" aria-hidden="true">#</a> \u4E11\u6570</h1><h2 id="\u6570\u5B66" tabindex="-1"><a class="header-anchor" href="#\u6570\u5B66" aria-hidden="true">#</a> \u6570\u5B66</h2><p>\u4ECE <code>5\u30013\u30012</code> \u4F9D\u6B21\u5904\u7406\u5373\u53EF\u3002</p><div class="language-typescript ext-ts"><pre class="language-typescript"><code><span class="token keyword">function</span> <span class="token function">isUgly</span><span class="token punctuation">(</span>n<span class="token operator">:</span> <span class="token builtin">number</span><span class="token punctuation">)</span><span class="token operator">:</span> <span class="token builtin">boolean</span> <span class="token punctuation">{</span>
+  <span class="token keyword">if</span> <span class="token punctuation">(</span>n <span class="token operator">&lt;=</span> <span class="token number">0</span><span class="token punctuation">)</span> <span class="token keyword">return</span> <span class="token boolean">false</span>
+  <span class="token keyword">const</span> uglyNumArr<span class="token operator">:</span> <span class="token builtin">number</span><span class="token punctuation">[</span><span class="token punctuation">]</span> <span class="token operator">=</span> <span class="token punctuation">[</span><span class="token number">5</span><span class="token punctuation">,</span> <span class="token number">3</span><span class="token punctuation">,</span> <span class="token number">2</span><span class="token punctuation">]</span>
+
+  <span class="token keyword">for</span> <span class="token punctuation">(</span><span class="token keyword">const</span> u <span class="token keyword">of</span> uglyNumArr<span class="token punctuation">)</span> <span class="token punctuation">{</span>
+    <span class="token keyword">while</span> <span class="token punctuation">(</span>n <span class="token operator">%</span> u <span class="token operator">===</span> <span class="token number">0</span><span class="token punctuation">)</span> <span class="token punctuation">{</span>
+      n <span class="token operator">/=</span> u
+    <span class="token punctuation">}</span>
+  <span class="token punctuation">}</span>
+  
+
+  <span class="token keyword">return</span> n <span class="token operator">===</span> <span class="token number">1</span>
+<span class="token punctuation">}</span>
+</code></pre></div>`,4);function t(o,e){return p}var u=s(a,[["render",t]]);export{u as default};
--- assets/command-shortcut.html.ecf1aad9.js
+++
@@ -1,1 +0,0 @@
-const t={key:"v-75b79a04",path:"/os/linux/command-shortcut.html",title:"\u547D\u4EE4\u884C\u5FEB\u6377\u952E",lang:"zh-CN",frontmatter:{title:"\u547D\u4EE4\u884C\u5FEB\u6377\u952E"},excerpt:"",headers:[],filePathRelative:"os/linux/command-shortcut.md",git:{updatedTime:1565020138e3,contributors:[{name:"shanyuhai123",email:"864299347@qq.com",commits:1}]}};export{t as data};
