2e36142304336b3dfadd23188ad9f51b5015abdc
shanyuhai123 <864299347@qq.com>
2022-08-21 22:04:12 +0800

    chore: change travellings domain

--- docs/.vuepress/config/navConf.js
+++ docs/.vuepress/config/navConf.js
@@ -57,5 +57,5 @@ module.exports = [
     { text: 'VuePress 侧边栏插件', link: 'https://shanyuhai123.github.io/vuepress-plugin-auto-sidebar/zh/' },
     { text: 'VuePress 官网', link: 'https://vuepress.vuejs.org/zh/' },
   ]},
-  { text: '🚇 开往', link: 'https://travellings.link' },
+  { text: '🚇 开往', link: 'https://travellings.cn' },
 ];
